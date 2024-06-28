0039e05f19abcf4bcce40cfbc2516696e260751c
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-28 21:58:06 +0700

    feat: add stateless concept

--- app/src/main/java/com/example/learn_jetpack_compose/state/StatelessCounter.kt
+++ app/src/main/java/com/example/learn_jetpack_compose/state/StatelessCounter.kt
@@ -5,9 +5,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.learn_jetpack_compose.ui.theme.LearnjetpackcomposeTheme
 
 @Composable
 fun StatelessCounter(
@@ -19,10 +25,36 @@ fun StatelessCounter(
         modifier = modifier.padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Button di klik $count kali")
+        Text(
+            text = "Button di klik $count kali",
+            modifier = modifier.padding(bottom = 8.dp)
+        )
         Button(onClick = { onClick() }) {
             Text(text = "Click Me")
         }
+    }
+}
+
+
+@Composable
+fun StatefulCounterRef(modifier: Modifier = Modifier) {
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    
+    StatelessCounter(
+        count = count,
+        onClick = { count++ },
+        modifier = modifier
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun StatefulCounterRefPrev() {
+    LearnjetpackcomposeTheme {
+        StatefulCounterRef()
     }
 }
 
